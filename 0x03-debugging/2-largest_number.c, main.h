b239941cc01f5f21936daddec1046d8ab6be9fa0
naol1 <81456232+naol1@users.noreply.github.com>
2021-09-03 17:49:02 +0300

    Update 2-largest_number.c, main.h

--- 0x03-debugging/2-largest_number.c, main.h
+++ 0x03-debugging/2-largest_number.c, main.h
@@ -1,28 +1,23 @@
-#include "holberton.h"
+ #include <stdio.h>
+#include "main.h"
 
 /**
- * largest_number - returns the largest of 3 numbers
- * @a: first integer
- * @b: second integer
- * @c: third integer
- * Return: largest number
- */
-int largest_number(int a, int b, int c)
+* main - prints the largest of 3 integers
+* Return: 0
+*/
+
+int main(void)
 {
-	int largest;
+        int a, b, c;
+        int largest;
 
-	if (a >= b && a >= c)
-	{
-		largest = a;
-	}
-	else if (b >= a && b >= c)
-	{
-		largest = b;
-	}
-	else if (c >= a && c >= b)
-	{
-		largest = c;
-	}
+        a = 972;
+        b = -98;
+        c = 0;
 
-	return (largest);
+        largest = largest_number(a, b, c);
+
+        printf("%d is the largest number\n", largest);
+
+        return (0);
 }
