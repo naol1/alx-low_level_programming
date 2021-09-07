cd2ef8a0ee677110a2a7c5157d91e20f49b77527
naol1 <81456232+naol1@users.noreply.github.com>
2021-09-07 22:14:40 +0300

    Add files via upload

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,21 +1,17 @@
 #include "holberton.h"
 
 /**
- * puts2 - print reverse characters.
+ * _puts - check the code for Holberton School students.
  * @str: validate the character
  * Return: Always 0.
  */
-void puts2(char *str)
+void _puts(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0' ; i++)
 	{
-		if (i % 2 == 0)
-		{
 		_putchar(str[i]);
-		}
-		i++;
 	}
 	_putchar('\n');
 }
