dda907aaeeb649be1e6fb228133f54ac21400266
Naol Tesfaye <81456232+naol1@users.noreply.github.com>
2022-03-12 23:14:34 +0300

    Add files via upload

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
+
 /**
- * Description: main - Prints out a string to stdout.
- * Return: 0 if success.
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
